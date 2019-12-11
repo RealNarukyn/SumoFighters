33b150f1f1c07618254ee07b164d3df5212b7a17
Narukyn <alexmusu99@gmail.com>
2019-12-11 19:01:52 +0100

    Cleaning Code

--- Sumo Figthers/Assets/Scripts/CharacterMovement.cs
+++ Sumo Figthers/Assets/Scripts/CharacterMovement.cs
@@ -9,14 +9,11 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     public float speed = 12f;
 
-    public float RotateSpeed = 30f;
-
     public float jumpHeight = 3.0f;
 
     Vector3 velocity;
     bool isGrounded;
    public Vector3 move;
-    //int angle_direction = 1;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -45,20 +42,5 @@ public class CharacterMovement : MonoBehaviour
 
         velocity += Physics.gravity * Time.deltaTime * 2f;
         controller.Move(velocity * Time.deltaTime);
-
-        
-        //if ( x < 0 && z > 0 || x < 0 && z < 0 )
-        //{
-        //    angle_direction = -1;
-        //}
-
-        //if (x > 0 && z > 0 || x > 0 && z < 0)
-        //{
-        //    angle_direction = 1;
-        //}
-
-        //transform.Rotate(Vector3.up * RotateSpeed * angle_direction * Time.deltaTime);
-
-        //   transform.Rotate(x < 0 ? -Vector3.up * RotateSpeed * Time.deltaTime : Vector3.up * RotateSpeed * Time.deltaTime);
     }
 }
--- Sumo Figthers/Assets/Scripts/CharacterRotation.cs
+++ Sumo Figthers/Assets/Scripts/CharacterRotation.cs
@@ -6,19 +6,13 @@ public class CharacterRotation : MonoBehaviour
 {
     [SerializeField]
     CharacterMovement controller;
-    
     [SerializeField]
-    float rotation_speed = 30f;
+    float rotation_speed = 5f;
 
-    float actualAngle = 0.0f;
     private void Update()
     {
-        //if (controller.transform.position.x > 0)
-        //    transform.Rotate(controller.transform.position.x < 0 ? -Vector3.up * rotation_speed * Time.deltaTime : Vector3.up * rotation_speed * Time.deltaTime);
-       
         if(controller.move != Vector3.zero)
-        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(controller.move),Time.deltaTime * rotation_speed);
-        //transform.Rotate(controller.transform.position.x < 0 ? -Vector3.up * rotation_speed * Time.deltaTime : Vector3.up * rotation_speed * Time.deltaTime);
+           transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(controller.move),Time.deltaTime * rotation_speed);
     }
 
 }
