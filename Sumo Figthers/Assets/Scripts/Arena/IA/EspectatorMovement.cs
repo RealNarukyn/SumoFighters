771815dc3b5d6715db20ee0826dd3dcbc497ce11
RealNarukyn <alexmusu99@gmail.com>
2020-01-10 02:53:26 +0100

    Added Espectators

---
+++ Sumo Figthers/Assets/Scripts/Arena/IA/EspectatorMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspectatorMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] materials;
+    [SerializeField]
+    private Rigidbody rb;
+
+    private float JumpHeight = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(new Vector3(0,0,0));
+
+        int randNumber = Random.Range(0, materials.Length);
+        GetComponent<MeshRenderer>().material = materials[randNumber];
+
+        int randomTime = Random.Range(0, 6);
+
+        InvokeRepeating("Jump", 2, randomTime);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
